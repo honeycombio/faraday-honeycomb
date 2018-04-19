66835b26538a9addec30c521f97d196df973372e
Sam Stokes <me@samstokes.co.uk>
2018-04-18 17:14:09 -0700

    support prototype tracing wackery

--- lib/faraday/honeycomb/middleware.rb
+++ lib/faraday/honeycomb/middleware.rb
@@ -1,5 +1,6 @@
 require 'faraday'
 require 'libhoney'
+require 'securerandom'
 
 require 'faraday/honeycomb/version'
 
@@ -23,7 +24,9 @@ module Faraday
         event.add_field :path, env.url.path
 
         start = Time.now
-        response = @app.call(env)
+        response = adding_span_metadata_if_available(event, env) do
+          @app.call(env)
+        end
 
         event.add_field :status, response.status
 
@@ -38,8 +41,27 @@ module Faraday
         if start && event
           finish = Time.now
           duration = finish - start
-          event.add_field :duration_ms, duration * 1000
+          event.add_field :durationMs, duration * 1000
           event.send
+        end
+      end
+
+      private
+      def adding_span_metadata_if_available(event, env)
+        return yield unless defined?(::Honeycomb.trace_id)
+
+        trace_id = ::Honeycomb.trace_id
+        name = "#{env.method} #{env.url.path}"
+
+        event.add_field :traceId, trace_id if trace_id
+        span_id = SecureRandom.uuid
+        event.add_field :id, span_id
+        event.add_field :serviceName, 'faraday'
+        event.add_field :name, name if name
+
+        ::Honeycomb.with_span_id(span_id) do |parent_span_id|
+          event.add_field :parentId, parent_span_id
+          yield
         end
       end
     end
