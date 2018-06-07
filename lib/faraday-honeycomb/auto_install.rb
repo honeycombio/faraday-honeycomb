bdb31d1ecb0b80eab647789f4d1dad07e459752b
Sam Stokes <sam@honeycomb.io>
2018-06-07 13:27:50 -0700

    log debug info on startup

--- lib/faraday-honeycomb/auto_install.rb
+++ lib/faraday-honeycomb/auto_install.rb
@@ -4,9 +4,13 @@ module Faraday
   module Honeycomb
     module AutoInstall
       class << self
-        def available?(**_)
-          gem 'faraday', ">= #{::Faraday::Honeycomb::MIN_FARADAY_VERSION}"
-        rescue Gem::LoadError
+        def available?(logger: nil)
+          constraint = ">= #{::Faraday::Honeycomb::MIN_FARADAY_VERSION}"
+          gem 'faraday', constraint
+          logger.debug "#{self.name}: detected Faraday #{constraint}, okay to autoinitialise" if logger
+          true
+        rescue Gem::LoadError => e
+          logger.debug "Didn't detect Faraday #{constraint} (#{e.class}: #{e.message}), not autoinitialising faraday-honeycomb" if logger
           false
         end
 
@@ -17,18 +21,19 @@ module Faraday
           Faraday::Connection.extend(Module.new do
             define_method :new do |*args, &orig_block|
               block = if orig_block
-                        proc do |b|
-                          b.use :honeycomb, client: honeycomb_client
-                          orig_block.call(b)
-                        end
-                      else
-                        proc do |b|
-                          b.use :honeycomb, client: honeycomb_client
-                          b.adapter Faraday.default_adapter
-                        end
-                      end
-              super(*args, &block).tap do
+                proc do |b|
+                  logger.debug "Adding Faraday::Honeycomb::Middleware in #{self}.new{}" if logger
+                  b.use :honeycomb, client: honeycomb_client
+                  orig_block.call(b)
+                end
+              else
+                proc do |b|
+                  logger.debug "Adding Faraday::Honeycomb::Middleware in #{self}.new" if logger
+                  b.use :honeycomb, client: honeycomb_client
+                  b.adapter Faraday.default_adapter
+                end
               end
+              super(*args, &block)
             end
           end)
         end
