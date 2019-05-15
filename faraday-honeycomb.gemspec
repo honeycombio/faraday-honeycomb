b1b087a0eda09636a27285058c62f4eedcbe7776
Matt Button <matt.button@geckoboard.com>
2019-05-15 16:37:32 +0100

    Change auto-install procedure to handle more edge cases
    Some popular open source gems (e.g. octokit) cache their rack middleware
    builder and reuse it across all connections. The first time this
    middleware builder is used the handlers it contains will be frozen, and
    can no longer be modified.
    
    If the `AutoInstall` logic attempts to append the Honeycomb middleware
    every time a connection is created then the app will error with an error
    like:
    
    ```
    Faraday::RackBuilder::StackLocked:
           can't modify middleware stack after making a request
    ```
    
    One thing to note is that when Honeycomb's `AutoInstall` logic handles the
    builder on the second connection the Honeycomb middleware is already
    present in the builder, so there's no need to add it a second time.
    
    The main change in this PR is that the `AutoInstall` logic will only
    insert the middleware into the builder if it isn't already there.
    
    Note that this will not prevent the `AutoInstall` logic erroring if a
    locked middleware builder is passed to Faraday, and the Honeycomb
    middleware is not present. This seems like more of an edge
    case/something the developer might want to be aware of, so I'm not going
    to handle it, but it should be quite easy to implement if needed.
    
    The second major change is that the `AutoInstall` logic will always
    attempt to add the middleware one level above the underlying HTTP
    adapter. Some Faraday middleware (e.g. retry, follow redirects) will
    cause Faraday to issue multiple HTTP requests in a single call.
    
    In the previous code Honeycomb's middleware ran before all other
    middleware, so it would miss these extra requests. By pushing the
    middleware as close as possible to the adapter we should be able to
    capture them.
    
    Moving the middleware insertion logic out of the monkey patched method
    allows us to collapse a lot of the if/else branches, which makes it a
    lot easier to understand.
    
    I've also tweaked the config parsing so that it handles the case where
    the first argument to `Faraday.new` is a string URL.
    
    Adding byebug/pry as a development dependency to make it easier to debug
    the `AutoInstall` logic.
    
    Fixes honeycombio/faraday-honeycomb#6

--- faraday-honeycomb.gemspec
+++ faraday-honeycomb.gemspec
@@ -24,6 +24,8 @@ Gem::Specification.new do |gem|
   gem.add_development_dependency 'rake'
   gem.add_development_dependency 'rspec'
   gem.add_development_dependency 'yard'
+  gem.add_development_dependency 'byebug'
+  gem.add_development_dependency 'pry'
 
   gem.files = Dir[*%w(
       lib/**/*
--- spec/auto_install_spec.rb
+++ spec/auto_install_spec.rb
@@ -9,25 +9,83 @@ RSpec.describe Faraday::Honeycomb::AutoInstall, auto_install: true do
 
   it "standard usage with no block works" do
     f = Faraday.new("http://honeycomb.io")
-    expect(f.builder.handlers).to eq([Faraday::Honeycomb::Middleware, Faraday::Request::UrlEncoded, Faraday::Adapter::NetHttp])
+    expect(f.builder.handlers).to eq([
+      Faraday::Request::UrlEncoded,
+      Faraday::Honeycomb::Middleware,
+      Faraday::Adapter::NetHttp
+    ])
   end
 
   it "providing a builder with string key works" do
     stack = Faraday::RackBuilder.new do |builder|
+      builder.request :retry
       builder.adapter Faraday.default_adapter
     end
     f = Faraday.new("builder" => stack)
-    expect(f.builder.handlers).to eq([Faraday::Honeycomb::Middleware, Faraday::Adapter::NetHttp])
+
+    expect(f.builder.handlers).to eq([
+      Faraday::Request::Retry,
+      Faraday::Honeycomb::Middleware,
+      Faraday::Adapter::NetHttp
+    ])
   end
 
   it "providing a builder with symbol key works" do
     stack = Faraday::RackBuilder.new do |builder|
+      builder.request :retry
       builder.adapter Faraday.default_adapter
     end
     f = Faraday.new(builder: stack)
-    expect(f.builder.handlers).to eq([Faraday::Honeycomb::Middleware, Faraday::Adapter::NetHttp])
+
+    expect(f.builder.handlers).to eq([
+      Faraday::Request::Retry,
+      Faraday::Honeycomb::Middleware,
+      Faraday::Adapter::NetHttp
+    ])
   end
 
+  it "providing a builder that only has an adapter works" do
+    stack = Faraday::RackBuilder.new do |builder|
+      builder.adapter Faraday.default_adapter
+    end
+    f = Faraday.new(builder: stack)
+
+    expect(f.builder.handlers).to eq([
+      Faraday::Honeycomb::Middleware,
+      Faraday::Adapter::NetHttp
+    ])
+  end
+
+  it "providing a builder AND url works" do
+    stack = Faraday::RackBuilder.new do |builder|
+      builder.request :retry
+      builder.adapter Faraday.default_adapter
+    end
+    f = Faraday.new("https://example.com", builder: stack)
+
+    expect(f.builder.handlers).to eq([
+      Faraday::Request::Retry,
+      Faraday::Honeycomb::Middleware,
+      Faraday::Adapter::NetHttp
+    ])
+  end
+
+  it "does not attempt to add honeycomb middleware if it already exists in the passed builder" do
+    stack = Faraday::RackBuilder.new do |builder|
+      builder.adapter Faraday.default_adapter
+    end
+    f = Faraday.new(builder: stack)
+    # force the builder to lock the middleware stack
+    f.builder.app
+
+    expect(f.builder.handlers).to eq([Faraday::Honeycomb::Middleware, Faraday::Adapter::NetHttp])
+
+    f2 = Faraday.new(builder: stack)
+    # force the builder to lock the middleware stack
+    f2.builder.app
+
+    expect(f2.builder.handlers).to eq([Faraday::Honeycomb::Middleware, Faraday::Adapter::NetHttp])
+  end
 
   it "providing a builder and a block works" do
     stack = Faraday::RackBuilder.new do |builder|
@@ -37,6 +95,12 @@ RSpec.describe Faraday::Honeycomb::AutoInstall, auto_install: true do
       faraday.request  :url_encoded
       faraday.adapter Faraday.default_adapter
     end
-    expect(f.builder.handlers).to eq([Faraday::Honeycomb::Middleware, Faraday::Response::Logger, Faraday::Request::UrlEncoded, Faraday::Adapter::NetHttp])
+
+    expect(f.builder.handlers).to eq([
+      Faraday::Response::Logger,
+      Faraday::Request::UrlEncoded,
+      Faraday::Honeycomb::Middleware,
+      Faraday::Adapter::NetHttp
+    ])
   end
 end
